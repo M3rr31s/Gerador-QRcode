58bd24d1e41018848cb3507c3e3ef93b14358341
M3rr31s <151941195+M3rr31s@users.noreply.github.com>
2024-01-24 14:44:52 -0300

    Subindo Projeto

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,45 @@
 const container = document.querySelector(".container");
-const qrCodeBtn = document.getElementById("#qr-form button")
+
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeIMG =  document.querySelector("#qr-code img")
 
 //Eventos
 
-function generateQrCode() {
-    console.log("teste")
+// Gerar QR Code
+function generaterQrCod() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return window.alert(`[ERRO] - FALTAM DADOS`);
+
+    qrCodeBtn.innerHTML = `Gerando QR Code...`
+
+    qrCodeIMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+
+    qrCodeIMG.addEventListener("load", () => {
+        container.classList.add("active");
+        qrCodeBtn.innerHTML = `Código Gerado!`
+    })
 }
 
 qrCodeBtn.addEventListener("click", () => {
-    generateQrCode()
+    generaterQrCod()
+})
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code == "Enter") {
+        generaterQrCod()
+    }
+})
+
+//Limpar Area do QRCode
+
+qrCodeInput.addEventListener("keyup", () => {
+    
+    if  (!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerHTML = `Gerar QRcode` ;   
+    }
+
 })
