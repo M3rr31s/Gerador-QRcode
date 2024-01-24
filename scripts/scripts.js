f59aa42cfaaaa06f21c34d73ec4dc8b792e2b3a0
M3rr31s <151941195+M3rr31s@users.noreply.github.com>
2024-01-24 12:13:44 -0300

    ajuste

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.getElementById("#qr-form button")
+
+//Eventos
+
+function generateQrCode() {
+    console.log("teste")
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode()
+})
